114f2e1afb9af47df3cc57bb9e3ec14fb440abf2
AETHER <95666615+Tarunr09@users.noreply.github.com>
2022-02-21 08:55:18 +0000

    .

---
+++ p3original.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int input()
+{
+  int n;
+  printf("Enter value:");
+  scanf("%d",&n);
+  return n;
+}
+int composite(int n)
+{
+  int a=0;
+  for (int i=2;i<=n/2;i++)
+  {
+   if(n%i==0){
+    a=1;
+    break;
+   }
+  }
+ return a;
+}
+void output(int a)
+{
+   if(a==1)
+   {
+   printf("It is a composite number \n");
+   }
+  else
+  {
+   printf("It is not a composite number  \n");
+  }
+}
+
+int main()
+{
+  int n,x;
+  n=input();
+  x=composite(n);
+  output(x);
+  return 0;
+}
---
+++ p4original.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+int input()
+{
+  int n;
+  printf("Enter value:");
+  scanf("%d",&n);
+  return n;
+}
+
+void input_array(int n, int a[n])
+{
+   int i;
+  printf("Enter the elements of the array:\n");
+  for(i=1;i<=n;i++){
+    scanf("%d",&a[i]);
+  }
+}
+
+int composite(int n )
+{
+    int a=0;
+  for (int i=2;i<=n/2;i++)
+  {
+   if(n%i==0){
+    a=1;
+    break;
+   }
+  }
+ return a;
+}
+  
+int sum_composite_numbers(int n, int a[n])
+{
+  int sum=0;
+  for(int i=1;i<=n;i++)
+  {
+    if(composite (a[i])==1)
+    {
+     sum=sum+a[i];
+    }
+  }
+  return sum;
+}
+
+void output(int sum)
+{
+  printf("The sum is %d",sum);
+}
+
+int main()
+{
+  int n;
+  n=input();
+  int a[n];
+  input_array(n,a);
+  int x;
+  x=sum_composite_numbers(n,a);
+  output(x);
+  return 0;
+}
