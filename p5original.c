84a5e8166adfd0a37bbde19d9370af23e09f72e6
AETHER <95666615+Tarunr09@users.noreply.github.com>
2022-04-01 04:47:41 +0000

    .

--- p5original.c
+++ p5original.c
@@ -1,4 +1,40 @@
 #include<stdio.h>
+int input()
+{
+  int n;
+  printf("Enter a number :\n");
+  scanf("%d",&n);
+  return n;
+}
+
+int gcd(int a, int b)
+{
+  int gcd;
+  for (int i=1;i<=a && i<=b; i++)
+    {
+      if(a%i==0 && b%i==0)
+      gcd=i;
+    }
+    return gcd;
+}
+void output(int a,int b,int gcd)
+{
+  printf("GCD for given two numbers is %d and %d is %d",a,b,gcd);
+}
+
+int main()
+{
+  int x,y,g;
+  x=input();
+  y=input();
+  g=gcd(x,y);
+  output(x,y,g);
+  return 0;
+}
+
+
+
+/*#include<stdio.h>
 int input(){
     int n;
     printf("Enter number to find gcd \n");
@@ -31,3 +67,4 @@ int main(){
     output(a,b,hcf);
     return 0;
 }
+*/
